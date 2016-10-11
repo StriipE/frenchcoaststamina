39958caf400e39c9b21d19972131e3ba7aac5882
Ewen Auffret <auffret.ewen@gmail.com>
2016-10-12 00:18:56 +0300

    New version of FCS website, using Angular this time, gotta go fast

---
+++ app/app.component.js
@@ -0,0 +1,10 @@
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'mainApp',
+      template: ''
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
