ecb4e051ec31f81618ade6a7e7586f67ff96dae9
Ewen Auffret <auffret.ewen@gmail.com>
2017-02-14 02:01:25 +0100

    Added Flags on Country display

--- routes/ranking.js
+++ routes/ranking.js
@@ -7,9 +7,10 @@ var router = express.Router();
 
 router.get('/high', function (req,res){
 
-    con.query('SELECT user.PlayerID, user.Name, user.CountryCode, COUNT(*) AS Played, ROUND(SUM(score_high_histo.FCSPoints),2) AS Score from scores_high ' +
+    con.query('SELECT user.PlayerID, user.Name, user.CountryCode, country.Name AS CountryName, country.Flag AS CountryFlag, COUNT(*) AS Played, ROUND(SUM(score_high_histo.FCSPoints),2) AS Score from scores_high ' +
               'INNER JOIN score_high_histo ON score_high_histo.ScoreID = scores_high.ScoreID ' +
               'INNER JOIN user ON user.PlayerID = scores_high.PlayerID ' +
+              'INNER JOIN country ON user.CountryCode = country.Code ' +
               'WHERE score_high_histo.ScoreID != 1 ' +
               'GROUP BY scores_high.PlayerID ORDER BY Score DESC', function(err,rows){
         if(err) throw err;
@@ -30,10 +31,11 @@ router.get('/high', function (req,res){
 
 router.get('/low', function (req,res){
 
-    con.query('SELECT user.PlayerID, user.Name, user.CountryCode, COUNT(*) AS Played, SUM(Fantastics) AS Fantastics, SUM(Excellents) AS Excellents, ' +
+    con.query('SELECT user.PlayerID, user.Name, user.CountryCode, country.Name AS CountryName, country.Flag AS CountryFlag, COUNT(*) AS Played, SUM(Fantastics) AS Fantastics, SUM(Excellents) AS Excellents, ' +
         'SUM(Greats) AS Greats, SUM(FCSPoints) AS Score from scores_low ' +
         'INNER JOIN score_low_optional_histo ON score_low_optional_histo.ScoreID = scores_low.ScoreID ' +
         'INNER JOIN user ON user.PlayerID = scores_low.PlayerID ' +
+        'INNER JOIN country ON user.CountryCode = country.Code ' +
         'WHERE score_low_optional_histo.ScoreID != 1 ' +
         'GROUP BY score_low_optional_histo.PlayerID ORDER BY Score DESC', function(err,rows){
         if(err) throw err;
